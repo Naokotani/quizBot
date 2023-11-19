2bf4d57d0e06621f065a01f1c141ffc0c8b1e2dc
Chris Hughes <naokotani@protonmail.com>
2023-11-19 05:15:56 -0400

    added autoselection to reload command

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,14 +9,53 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('category')
 				.setDescription('The category of the command.')
+				.setAutocomplete(true)
 				.setRequired(true))
 		.addStringOption(option =>
 			option.setName('command')
 				.setDescription('The command to reload.')
+				.setAutocomplete(true)
 				.setRequired(true)),
+	async autocomplete(interaction) {
+
+		const foldersPath = path.join(__dirname, '../../commands');
+		const commandFolders = fs.readdirSync(foldersPath);
+		let commandFiles;
+
+		for (const folder of commandFolders) {
+			const commandsPath = path.join(foldersPath, folder);
+			commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+		};
+
+		let trimmedFiles = [];
+
+		for (const file of commandFiles) {
+			let split = file.split('.');
+			split.pop();
+			let finalName = split.join(".");
+			trimmedFiles.push(finalName);
+		}
+
+		const focusedOption = interaction.options.getFocused(true);
+
+		let choices
+
+		if (focusedOption.name === 'category') {
+			choices = commandFolders;
+		}
+
+		if (focusedOption.name === 'command') {
+			choices = trimmedFiles;
+		}
+
+
+		const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+		await interaction.respond(
+			filtered.map(choice => ({ name: choice, value: choice })),
+		);
+	},
 	async execute(interaction) {
 		const categoryName = interaction.options.getString('category', true).toLowerCase();
-		console.log(categoryName);
 		const commandName = interaction.options.getString('command', true).toLowerCase();
 		const command = interaction.client.commands.get(commandName);
 
